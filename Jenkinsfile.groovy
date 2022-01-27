a4cd2bcc4a269f855d37c76bb74af345e692acae
ksreerao <95680358+ksreerao@users.noreply.github.com>
2022-01-27 11:51:20 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,119 +1,113 @@
-def mvnHome\
-def remote = [ : ]\
-	remote.name = \'91deploy\'92\
-	remote.host = \'92192.168.122.1\'92\
-	//remote.user = \'91root\'92\
-	//remote.password = \'91datta\'92\
-	remote.allowAnyHosts = true\
-pipeline\
-\{\
-	agent none\
-	stages\
-	\{\
-		stage (\'91Preparation\'92) 	\
-		\{\
-			agent \
-			\{\
-				label \'91master\'92	\
-			\}\
-			steps \
-			\{\
-				git \'91https://github.com/ksreerao/Maven.git'	\
-				stash \'92Source\'92\
-				script \
-				\{\
-					mvnHome = tool \'91LocalMaven\'92\
-				\}\
-			\}\
-		\}\
-		stage (\'91build\'92) \
-		\{\
-			agent \
-			\{\
-				label \'91master\'92\
-\
-			\}\
-			steps\
-			\{\
-				sh \'91 $\{master\}/bin/mvn clean package \'92\
-			\}\
-			post\
-			\{\
-				always\
-				\{\
-					junit \'91target/surefire-reports/*.xml\'92\
-					archiveArtifacts \'91**/*.war\'92\
-					fingerprint \'91**/*.war\'92\
-				\}\
-			\}\
-		\}\
-		stage(\'91Deploy-to-Stage\'92) \
-		\{\
-			agent\
-			\{\
-				label \'91master\'92\
-			\}\
-			steps \
-			\{\
-				sshPut remote: remote, from: \'91target/java-maven-1.0.war\'92, into: \'91/workspace/nexusServer/webapps\'92\
-			\}\
-		\}\
-		stage (\'91Integration-Test\'92) \
-		\{\
-			agent\
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
-\cf0 			\{\
-				label \'91master\'92\
-			\}\
-			steps \
-			\{\
-				parallel (\
-					\'91integration\'92 : \{\
-						unstash \'91Source\'92\
-						sh \'91 $\{mvnHome\}/bin/mvn clean verify \'91\
-						\},	\'91quality\'92 : \{\
-								unstash \'91Source\'92\
-								sh \'91 $\{mvnHome\}/bin/mvn clean test \'91\
-							\}\
-				)\
-			\}\
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
-\cf0 		\}\
-		stage (\'91approve\'92)\
-		\{\
-			agent \
-			\{\
-				label \'91master\'92\
-			\}\
-			steps \
-			\{\
-				timeout(time: 7, unit: \'91DAYS\'92 )\
-				\{\
-					input message: \'91Do you want to deploy?\'92 , submitter: \'91admin\'92\
-				\}\
-			\}\
-		\}\
-		stage (\'91Prod-Deploy\'92) \
-		\{\
-			agent \
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
-\cf0 			\{\
-				label \'91master\'92\
-			\}\
-			steps \
-			\{	\
-				unstash \'92Source\'92\
-				sh \'91 $\{mvnHome\}/bin/mvn clean package \'91\
-			\}\
-			post\
-			\{\
-				always\
-				\{\
-					archiveArtifacts \'91**/*.war\'92\
-				\}\
-			\}\
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
-\cf0 		\}\
-	\}\
-\}\
+def mvnHome
+def remote = [ : ]
+	remote.name = 'deploy'
+	remote.host = '92192.168.122.1'
+	//remote.user = 'root'
+	//remote.password = 'datta'
+	remote.allowAnyHosts = true
+pipeline
+{
+	agent none
+	stages
+	{
+		stage ('Preparation') 	
+		{
+			agent
+			{
+				label 'master'
+			}
+			steps 
+			{
+				git 'https://github.com/ksreerao/Maven.git'
+				stash 'Source'
+				script 
+				{
+					mvnHome = tool 'LocalMaven'
+				}
+			}
+		}
+		stage ('build') 
+		{
+			agent 
+			{
+				label 'master'
+			}
+			steps
+			{
+				sh ' ${master}/bin/mvn clean package '
+			}
+			post
+			{
+				always
+				{
+					junit 'target/surefire-reports/*.xml'
+					archiveArtifacts '**/*.war'
+					fingerprint '**/*.war'
+				}
+			}
+		}
+		stage('Deploy-to-Stage') 
+		{
+			agent
+			{
+				label 'master'
+			}
+			steps 
+			{
+				sshPut remote: remote, from: 'target/java-maven-1.0.war', into: '/workspace/nexusServer/webapps'
+			}
+		}
+		stage ('Integration-Test') 
+		{
+			agent
+			{
+				label 'master'
+			}
+			steps 
+			{
+				parallel (
+					'integration' : {
+						unstash 'Source'
+						sh ' ${mvnHome}/bin/mvn clean verify '
+						},	'quality' : {
+								unstash 'Source'
+								sh ' ${mvnHome}/bin/mvn clean test '
+							}
+				)
+			}
+		}
+		stage ('approve')
+		{
+			agent 
+			{
+				label 'master'
+			}
+			steps 
+			{
+				timeout(time: 7, unit: 'DAYS')
+				{
+					input message: 'Do you want to deploy?' , submitter: 'admin'
+				}
+			}
+		}
+		stage ('Prod-Deploy') 
+		{
+			agent 
+			{
+				label 'master'
+			}
+			steps 
+			{	
+				unstash 'Source'
+				sh ' ${mvnHome}/bin/mvn clean package '
+			}
+			post
+			{
+				always
+				{
+					archiveArtifacts '**/*.war'
+				}
+			}
+		}
+	}
 }
